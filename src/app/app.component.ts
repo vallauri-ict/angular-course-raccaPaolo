e28b0b645812270823d34e5f0cf75380c2028145
Paolo Racca <p.racca.1368@vallauri.edu>
2021-10-07 10:47:43 +0200

    [COMPONENT] Changed the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-course';
-  public STUDENTS = ['De Toffol', 'Valinotto', 'Dellavalle','Racca'];
 }
--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'student-component',
   templateUrl: './student.component.html',
-  styleUrls: ['./student.component.css']
+  styleUrls: ['./student.component.css'],
 })
 export class StudentComponent implements OnInit {
-  @Input() STUDENT:string;
-  constructor() { }
-
-  ngOnInit(): void {
+  public STUDENTS = [
+    { name: 'De Toffol', hobby: 'Karate', gender: 'M' },
+    { name: 'Valinotto', hobby: 'Paddle', gender: 'F' },
+    { name: 'Dellavalle', hobby: 'Poligono', gender: 'M' },
+    { name: 'Racca', hobby: 'Paddle', gender: 'M' },
+    { name: 'Luna', hobby: 'Truccarsi', gender: 'F' },
+    { name: 'Kekka', hobby: 'Paddle', gender: 'F' },
+    { name: 'Beppa', hobby: 'Tedeschiare', gender: 'F' },
+    { name: 'Lorenzo', hobby: 'Fotografia', gender: 'M' },
+    { name: 'Mommo', hobby: 'Dormire', gender: 'F' },
+    { name: 'Parrick', hobby: 'Cucinare', gender: 'M' },
+    { name: 'Marghe', hobby: 'Correre', gender: 'F' },
+  ];
+  student: {name:string,hobby:string,gender:string};
+  constructor() {
+    this.student = this.STUDENTS[Math.floor(Math.random() * this.STUDENTS.length)];
   }
 
+  ngOnInit(): void {}
 }
